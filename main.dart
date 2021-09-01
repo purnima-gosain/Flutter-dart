631f5cbb54f4902a3e44ed536464ef7a1a837503
purnima-poo <np03a190173@herladcollege.edu.np>
2021-09-01 22:11:47 +0545

    arithmetic, unary and assignment operators and datatypes

--- main.dart
+++ main.dart
@@ -1,4 +1,39 @@
 void main() {
   //declares the function called main
-  print("Hello World!");
+  var name = "Poo";
+  var rollNo = 29;
+  var heart = '\u2665'; //dart runes
+  var laugh = '\u{1f600}';
+  int a = 23;
+  int b = 19;
+  int c = 13;
+  int d = 11;
+  int e = 12;
+  int f = 10;
+  int g = 110;
+  print("My name is ${name} and my roll number is ${rollNo}.");
+  print(heart);
+  print(laugh);
+  print("Examples of Arithmetic Operators:");
+  print("a+b = ${a + b}");
+  print("a-b = ${a - b}");
+  print("a*b = ${a * b}");
+  print("a/b = ${a / b}");
+  print("a%b = ${a % b}");
+  print("Examples of Unary Operators:");
+  print(a++);
+  print(++b);
+  print(--c);
+  print(d--);
+  print("Examples of Assignment Operators:");
+  e += f;
+  print("e+=f = ${e}");
+  g -= f;
+  print("g-=f = ${g}");
+  e *= f;
+  print("e*=f = ${e}");
+  e ~/= f;
+  print("e~/=f = ${e}");
+  e %= f;
+  print("e%=f = ${e}");
 }
