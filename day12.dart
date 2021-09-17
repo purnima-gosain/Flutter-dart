fa217e2549d199d26acf0cf4ea7f314a9049186d
purnima-poo <np03a190173@herladcollege.edu.np>
2021-09-17 17:19:40 +0545

    day 12

---
+++ day12.dart
@@ -0,0 +1,16 @@
+void main (){
+  print("This is map lateral:");
+  var information = {'Name':'Poo','Id':'2056'};
+  print(information);
+  //for adding values to map
+  information['Address']='Ktm';
+  print("After adding value in map");
+  print(information);
+  
+  print("This is Map constructor:");
+  var info = new Map();
+  info['Name']='Purnima';
+  info['Id']='2057';
+  info['Address']='Nepalgunj';
+  print(info);
+}
