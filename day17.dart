8374e826f8b350ccb2719cd743fa8fccd19e85e5
purnima-poo <np03a190173@herladcollege.edu.np>
2021-09-23 07:50:03 +0545

    day17

--- day17.dart
+++ day17.dart
@@ -1,3 +1,4 @@
+
 class Person{
   var name;
   var age;
@@ -11,10 +12,17 @@ class Person{
 }
 
 void main(){
+  Bike bike = new Bike.model('Yamaha');
   Person info = new Person();
   info.name ="Poo";
   info.age = 21;
   info.address="Nepalgunj";
 
   info.viewPersonInfo();
+}
+//constructor
+class Bike{
+  Bike.model(String model){
+    print("This is ${model}");
+  }
 }
