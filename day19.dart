384ab07692c1ab7ce53cca6b5d463e5fc5fd5d90
purnima-poo <np03a190173@herladcollege.edu.np>
2021-09-24 07:36:58 +0545

    day19

---
+++ day19.dart
@@ -0,0 +1,23 @@
+//defining a class
+class Module{
+    var moduleId;
+    var moduleName;
+    var moduleLeader;
+
+//making class function
+    viewModuleDetail(){
+      print("Module Id : ${moduleId}");
+      print("Module Name : ${moduleName}");
+      print("Module Leader : ${moduleLeader}");
+    }
+  }
+
+  void main(){
+  //creating an object called obj
+  var obj = new Module();
+  obj.moduleId="5C601ES";
+  obj.moduleName="Computer Science";
+  obj.moduleLeader = "Tom";
+//accessing class function
+  obj.viewModuleDetail();
+  }
