14747b91deb717cd3693f848461ff64ffd7bdf65
purnima-poo <np03a190173@herladcollege.edu.np>
2021-10-11 08:48:06 +0545

    day35

---
+++ day35.dart
@@ -0,0 +1,10 @@
+void main() {
+
+
+  String name = "Solutionisamixture";
+  String message="Hello1My2name3is4Purnima";
+
+  print(name.split(""));
+  print(message.split(new RegExp(r"[0-5]")));
+  
+}
