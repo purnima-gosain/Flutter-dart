bdde3f0f85c18a64e21b7423e3bdb36e74e114bb
purnima-poo <np03a190173@herladcollege.edu.np>
2021-10-16 16:37:43 +0545

    day41

---
+++ day41.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+void main(){
+  print("Enter the number of rows:");
+  int rows = int.parse(stdin.readLineSync()!);
+  int i,j,space;
+  for(i=1;i<=rows;i++){
+    for(space=1;space<=(rows-i);space++){
+      stdout.write(" ");
+    }
+    for(j=1;j<=i;j++){
+      stdout.write("*");
+
+    }
+    stdout.writeln();
+  }
+}
